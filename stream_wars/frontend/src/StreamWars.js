3d0e8dbcf788d6ff71a2dcaccf6d5d43b44df80a
Murilo Cunha <murilo@dataroots.io>
2022-08-16 22:48:11 +0300

    rename file to reflect component

---
+++ stream_wars/frontend/src/StreamWars.js
@@ -0,0 +1,74 @@
+import Crawl from "./Crawl";
+import { useRenderData } from "streamlit-component-lib-react-hooks";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement,
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function StreamWars() {
+  const renderData = useRenderData();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div
+      style={{
+        height: "700px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="outlined" onClick={handleClickOpen}>
+        {renderData.args["button_text"]}
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative", backgroundColor: "black" }}>
+          <Toolbar style={{ display: "flex", justifyContent: "end" }}>
+            <IconButton
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Crawl
+          intro={renderData.args["intro"]}
+          title={renderData.args["title"]}
+          episodeNumber={renderData.args["episode_number"]}
+          episodeTitle={renderData.args["episode_title"]}
+          content={"this is\n\nthe test"}
+        />
+      </Dialog>
+    </div>
+  );
+}
