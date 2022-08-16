e0166607997681215bcf196d01df207d47e7158d
Murilo Cunha <murilo@dataroots.io>
2022-08-16 22:45:23 +0300

    add component example

--- stream_wars/__init__.py
+++ stream_wars/__init__.py
@@ -1,32 +1,26 @@
 import os
 import streamlit.components.v1 as components
 from pathlib import Path
+from streamlit.components.v1.components import (
+    CustomComponent,
+)
 
-_RELEASE = os.getenv("ST_IS_RELEASE", False)
+_RELEASE = os.getenv("STREAMLIT_RELEASE", False)
 
-if not _RELEASE:
-    _stream_wars = components.declare_component(
-        "stream_wars",
-        url="http://localhost:3001",
-    )
-else:
+if _RELEASE:
     build_dir = Path(__file__).parent / "frontend" / "build"
     _stream_wars = components.declare_component("stream_wars", path=build_dir)
+else:
+    _stream_wars = components.declare_component(
+        "stream_wars",
+        url="http://localhost:3000",
+    )  
 
-def stream_wars(name, key=None):
-    return _stream_wars(name=name, key=key, default=0)
+def stream_wars(button_text: str, *, intro: str, episode_number: str, episode_title: str, content: str, key: str=None) -> CustomComponent:
+    return _stream_wars( button_text=button_text, intro=intro, episode_number=episode_number, episode_title=episode_title, content=content, key=key)
 
 if __name__ == "__main__":
     import streamlit as st
 
-    st.subheader("Component with constant args")
-    # num_clicks = stream_wars("World")
-    # st.markdown("You've clicked %s times!" % int(num_clicks))
-
-    st.markdown("---")
-    st.subheader("Component with variable args")
-
-    
-    name_input = st.text_input("Enter a name", value="Streamlit")
-    num_clicks = 2 #my_component(name_input, key="foo")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+    st.title("Stream Wars")
+    stream_wars("Tell me a story", intro= "Not that long ago, in an office far, far away...", title="dataroddit", episode_number="Episode 0", episode_title="THE RISE OF DATA", content="""This is\na test""", key=None)
